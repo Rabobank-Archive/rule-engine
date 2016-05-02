5409cd2f71037bbf60ef211deb899b7f1882e506
Vincent Zorge <V.Zorge@Linuse.nl>
2016-05-02 23:29:01 +0200

    Revert accidental rename

--- engine/src/main/scala/org/scalarules/dsl/nl/grammar/package.scala
+++ engine/src/main/scala/org/scalarules/dsl/nl/grammar/package.scala
@@ -4,6 +4,6 @@ package org.scalarules.dsl.nl
   * Aggregates the keywords and implicit definitions of the Scala-Rules DSL. Import this package's members to
   * use the DSL in your files.
   */
-package object grammar extends ScalaRulesDslTrait {
+package object grammar extends ScalaRulesDsl {
 
 }
