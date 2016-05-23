ee67b7a8c4b20f6aa3768f8425bd64ec7f6169f9
Jan-Hendrik Kuperus <jhkuperus@yoink.nl>
2016-05-23 13:29:01 +0200

    Removed valueClass. Usage of reflection caused troubles in WebSphere and feature was not used anyway.

--- engine-core/src/main/scala/org/scalarules/engine/facts.scala
+++ engine-core/src/main/scala/org/scalarules/engine/facts.scala
@@ -10,35 +10,16 @@ trait Fact[+A] {
   def description: String
 
   def toEval: Evaluation[A]
-  def valueClass: Class[_]
 }
 
 case class SingularFact[+A : TypeTag](name: String, isResult: Boolean = true, description: String = "") extends Fact[A] {
   def toEval: Evaluation[A] = new SingularFactEvaluation(this)
 
   override def toString: String = name
-
-  override def valueClass: Class[_] = {
-    val tag = implicitly[TypeTag[A]]
-
-    tag.mirror.runtimeClass(tag.tpe)
-  }
 }
 
 case class ListFact[+A : TypeTag](name: String, isResult: Boolean = true, description: String = "") extends Fact[List[A]] {
   def toEval: Evaluation[List[A]] = new ListFactEvaluation[A](this)
 
   override def toString: String = name
-
-  override def valueClass: Class[_] = {
-    val tag = implicitly[TypeTag[A]]
-
-    tag.mirror.runtimeClass(resolveTypeTagToElementType(tag.tpe))
-  }
-
-  @tailrec
-  private def resolveTypeTagToElementType(tpe: Type): Type = tpe.typeArgs match {
-    case t :: ts => resolveTypeTagToElementType(t)
-    case Nil => tpe
-  }
 }
