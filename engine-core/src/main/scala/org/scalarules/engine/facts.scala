b2c096db00d83535d71675f5e9e16433769d1217
Jan-Hendrik Kuperus <jhkuperus@yoink.nl>
2016-05-24 07:16:41 +0200

    Removed the TypeTag altogether from Facts

--- engine-core/src/main/scala/org/scalarules/engine/facts.scala
+++ engine-core/src/main/scala/org/scalarules/engine/facts.scala
@@ -1,8 +1,6 @@
 package org.scalarules.engine
 
-import scala.annotation.tailrec
 import scala.language.existentials
-import scala.reflect.runtime.universe._
 
 trait Fact[+A] {
   def name: String
@@ -12,13 +10,13 @@ trait Fact[+A] {
   def toEval: Evaluation[A]
 }
 
-case class SingularFact[+A : TypeTag](name: String, isResult: Boolean = true, description: String = "") extends Fact[A] {
+case class SingularFact[+A](name: String, isResult: Boolean = true, description: String = "") extends Fact[A] {
   def toEval: Evaluation[A] = new SingularFactEvaluation(this)
 
   override def toString: String = name
 }
 
-case class ListFact[+A : TypeTag](name: String, isResult: Boolean = true, description: String = "") extends Fact[List[A]] {
+case class ListFact[+A](name: String, isResult: Boolean = true, description: String = "") extends Fact[List[A]] {
   def toEval: Evaluation[List[A]] = new ListFactEvaluation[A](this)
 
   override def toString: String = name
