1d4c44f668b1f5cdd20938cd08eef7ec37e30fcb
Jan-Hendrik Kuperus <jhkuperus@yoink.nl>
2016-07-04 14:48:57 +0200

    Removed old Glossary

--- engine/src/main/scala/org/scalarules/utils/GlossaryConverter.scala
+++ engine/src/main/scala/org/scalarules/utils/GlossaryConverter.scala
@@ -6,11 +6,6 @@ import scala.reflect.ClassTag
 
 object GlossaryConverter {
 
-  @deprecated(message = "Glossary has been replaced with MacroGlossary", since = "0.3.0")
-  def toJson(g: Glossary): String = {
-    g.getFacts.map( factToJson ).mkString("{", ", ", "}")
-  }
-
   def toJson(g: MacroGlossary): String = {
     g.facts.map( factToJson ).mkString("{", ", ", "}")
   }
