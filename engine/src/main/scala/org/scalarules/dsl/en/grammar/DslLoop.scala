851d866890dad0691e0c8106f1eadb513e962053
Vincent Zorge <V.Zorge@Linuse.nl>
2016-08-30 21:37:22 +0200

    First translation of DslLoop, with use of abstract class to reduce code duplication

--- engine/src/main/scala/org/scalarules/dsl/en/grammar/DslLoop.scala
+++ engine/src/main/scala/org/scalarules/dsl/en/grammar/DslLoop.scala
@@ -1,4 +1,4 @@
-package org.scalarules.dsl.nl.grammar
+package org.scalarules.dsl.en.grammar
 
 import org.scalarules.dsl.core.grammar.{AbstractDslLoop, DslEvaluation}
 import org.scalarules.engine._
@@ -18,11 +18,11 @@ class DslLoopOverWord[I](inputList: ListFact[I]) {
 }
 
 class DslLoop[I](inputList: ListFact[I], contextAddition: I => Context) extends AbstractDslLoop(inputList, contextAddition) {
-  def doe[O](dslEvaluation: DslEvaluation[O]): SubRunData[O, I] = super.simpleEvaluation(dslEvaluation)
-  def doe[O, C](subRunData: SubRunData[O, C]): SubRunData[List[O], I] = super.simpleSubRunData(subRunData)
-  def geeft[O](resultFact: Fact[O]): SubBerekening[I, O] = new SubBerekening(inputList, contextAddition, resultFact)
+  def perform[O](dslEvaluation: DslEvaluation[O]): SubRunData[O, I] = super.simpleEvaluation(dslEvaluation)
+  def perform[O, C](subRunData: SubRunData[O, C]): SubRunData[List[O], I] = super.simpleSubRunData(subRunData)
+  def returns[O](resultFact: Fact[O]): SubBerekening[I, O] = new SubBerekening(inputList, contextAddition, resultFact)
 }
 
 class SubBerekening[I, O](inputList: ListFact[I], contextAddition: I => Context, yieldFact: Fact[O]) {
-  def door(berekening: Berekening): SubRunData[O, I] = new SubRunData(berekening.berekeningen, contextAddition, inputList, yieldFact)
+  def through(calculation: Calculation): SubRunData[O, I] = new SubRunData(calculation.derivations, contextAddition, inputList, yieldFact)
 }
